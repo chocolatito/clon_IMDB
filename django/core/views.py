3b0c3b7e74001ae708b995e517c6fdfe53a22cf0
chocolatito <sal_defrutas@hotmail.com>
2020-01-26 20:24:37 -0300

    Finalización del capítulo 1

--- django/core/views.py
+++ django/core/views.py
@@ -1,3 +1,27 @@
 from django.shortcuts import render
 
+from django.views.generic import ListView, DetailView
+from .models import Movie, Person
+
 # Create your views here.
+
+
+class MovieDetail(DetailView):
+    # model = Movie
+    queryset = (Movie.objects.all_with_related_persons())
+
+
+class MovieList(ListView):
+    model = Movie
+    paginate_by = 2
+
+
+class PersonDetail(DetailView):
+    queryset = Person.objects.all_with_prefetch_movies()
+
+
+class PersonList(ListView):
+    model = Person
+    paginate_by = 2
+
+# Setting the order
